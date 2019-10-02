06eeb262e96c579890799784a1e92adfd03e8d01
Oldie55 <oliver.dietsche@gmail.com>
2019-10-02 09:23:31 +0200

    Remove spring security temporarily

--- src/main/java/ch/zli/financemanager/controller/PrivateController.java
+++ src/main/java/ch/zli/financemanager/controller/PrivateController.java
@@ -1,16 +1,16 @@
-package ch.zli.financemanager.controller;
-
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/api/private")
-public class PrivateController {
-
-    @GetMapping
-    public String getMessage(UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken) {
-        return "Hello from private API controller";
-    }
-}
+//package ch.zli.financemanager.controller;
+//
+//import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//@RequestMapping("/api/private")
+//public class PrivateController {
+//
+//    @GetMapping
+//    public String getMessage(UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken) {
+//        return "Hello from private API controller";
+//    }
+//}
