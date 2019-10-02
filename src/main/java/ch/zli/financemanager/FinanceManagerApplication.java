9da59c6c5ca707d96f20ff9ac06dcfebbb0a95b4
Oldie55 <oliver.dietsche@gmail.com>
2019-10-02 09:14:57 +0200

    Initial setup

---
+++ src/main/java/ch/zli/financemanager/FinanceManagerApplication.java
@@ -0,0 +1,12 @@
+package ch.zli.financemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinanceManagerApplication.class, args);
+    }
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,18 @@
+insert into project (id, title)
+values  (1, 'Schule');
+
+insert into category (id, title)
+values  (1, 'Material'),
+        (2, 'Ausfluege');
+
+insert into payment (id, amount, title, `date`, description, project_id, category_id)
+values  (1, 49.95, 'Formelsammlung', TO_DATE('02/10/2019', 'DD/MM/YYYY'), 'Eine Formelsammlung fuer Mathematik und Physik', 1, 1);
+
+insert into user (id, `name`, password)
+values  (1, 'olidie', 'asdfasdf');
+
+insert into role (id, `name`)
+values  (1, 'ADMIN');
+
+insert into user_roles (users_id, roles_id)
+values (1, 1);
